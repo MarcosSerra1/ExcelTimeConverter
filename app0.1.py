fec798306a78611c451cf2e8cf69b6ee2a3c1ce0
Marcos Serra <marcosserra133@gmail.com>
2023-09-11 16:20:34 -0300

    Atualização caminho planilha
    atualização do caminho da planilha e novas bibliotecas

--- app0.1.py
+++ app0.1.py
@@ -1,9 +1,24 @@
-# Ler dados da planilha 
 import openpyxl
+import time
+import os
+import sys
 
-# Abrir planilha
-planilha = openpyxl.load_workbook('CONVERSAO_HORAS_DECIMAIS_app1.xlsx')
-# Abrir pagina da planilha
+# Obtém o diretório do executável, onde o script está sendo executado
+exec_dir = os.path.dirname(sys.argv[0])
+
+# Constrói o caminho completo para a planilha
+planilha_path = os.path.join(exec_dir, 'CONVERSAO_HORAS_DECIMAIS_app1.xlsx')
+
+# Verifique se o arquivo da planilha existe
+if not os.path.exists(planilha_path):
+    print('=' * 65)
+    print(f'O arquivo {planilha_path} não foi encontrado.')
+    print('=' * 65)
+    time.sleep(5)
+    sys.exit(1)
+
+# Abrir a planilha
+planilha = openpyxl.load_workbook(planilha_path)
 conversao_sheet = planilha['conversao']
 
 # Planilha de evento_simplificado.xls
@@ -33,4 +48,7 @@ for linha in conversao_sheet.iter_rows(min_row=2):
 # Salvar a planilha com os valores calculados
 planilha.remove(conversao_sheet) # Excluindo planilha conversão
 planilha.save('evento_simplificado.xlsx')
-print('Valores calculados salvos na planilha "evento_simplificado.xls"')
+print('=' * 65)
+print('Valores calculados salvos na planilha "evento_simplificado.xlsx"')
+print('=' * 65)
+time.sleep(5)
