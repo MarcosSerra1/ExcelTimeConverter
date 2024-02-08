f308b241422997293ea1b992e486ec51cc0e7aee
Marcos Serra <marcosserra133@gmail.com>
2024-02-08 16:47:47 -0300

    Atualização main.py
    atualização das constantes

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import tkinter as tk
 from utils.calcular_horas_decimais import calcular_horas_decimais
 
 # Configuração da janela principal
-root = tk.Tk()
-root.title("Calculadora de Horas Decimais")
-root.geometry("400x200")  # Definindo o tamanho da janela
+ROOT = tk.Tk()
+ROOT.title("Calculadora de Horas Decimais")
+ROOT.geometry("400x200")  # Definindo o tamanho da janela
 
 # Botão para iniciar o cálculo
-calcular_button = tk.Button(root, text="Calcular Horas Decimais", command=calcular_horas_decimais)
-calcular_button.pack(pady=20)
+BUTAO_CALCULAR = tk.Button(ROOT, text="Calcular Horas Decimais", command=calcular_horas_decimais)
+BUTAO_CALCULAR.pack(pady=20)
 
 # Botão para finalizar a aplicação
-fechar_button = tk.Button(root, text="Fechar", command=root.destroy)
-fechar_button.pack(pady=10)
+BOTAO_FECHAR = tk.Button(ROOT, text="Fechar", command=ROOT.destroy)
+BOTAO_FECHAR.pack(pady=10)
 
 # Iniciar o loop principal da interface gráfica
-root.mainloop()
+ROOT.mainloop()
