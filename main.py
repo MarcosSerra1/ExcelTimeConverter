1cf2d083f1871533e5f3cdd3c87538cd3eae73f3
Marcos Serra <marcosserra133@gmail.com>
2024-02-08 16:49:58 -0300

    Atualização main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import tkinter as tk
 from utils.calcular_horas_decimais import calcular_horas_decimais
 
 # Configuração da janela principal
-ROOT = tk.Tk()
-ROOT.title("Calculadora de Horas Decimais")
-ROOT.geometry("400x200")  # Definindo o tamanho da janela
+root = tk.Tk()
+root.title("Calculadora de Horas Decimais")
+root.geometry("400x200")  # Definindo o tamanho da janela
 
 # Botão para iniciar o cálculo
-BUTAO_CALCULAR = tk.Button(ROOT, text="Calcular Horas Decimais", command=calcular_horas_decimais)
-BUTAO_CALCULAR.pack(pady=20)
+botao_calcular = tk.Button(root, text="Calcular Horas Decimais", command=calcular_horas_decimais)
+botao_calcular.pack(pady=20)
 
 # Botão para finalizar a aplicação
-BOTAO_FECHAR = tk.Button(ROOT, text="Fechar", command=ROOT.destroy)
-BOTAO_FECHAR.pack(pady=10)
+botao_fechar = tk.Button(root, text="Fechar", command=root.destroy)
+botao_fechar.pack(pady=10)
 
 # Iniciar o loop principal da interface gráfica
-ROOT.mainloop()
+root.mainloop()
